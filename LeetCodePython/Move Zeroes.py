2c0889e079d7fcbf8489bd5d6017b3a3a23ca636
Andrea Bellu <bellu.andrea@outlook.it>
2023-01-30 14:26:16 +0100

    Delete LeetCodePython directory

--- LeetCodePython/Move Zeroes.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        p = 0
-        
-        for element in range(len(nums)):
-            if nums[element]:
-                nums[p], nums[element] = nums[element], nums[p]
-                p += 1
-        return nums
